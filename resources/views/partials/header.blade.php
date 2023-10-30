f19d5189f1e38ae482fc0357edbcfd8f3e4fc4e9
DaniloSalerno <salerno.danilo@hotmail.it>
2023-10-30 14:26:31 +0100

    initial commit
    add header and footer partials with placeholder content

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,8 @@
+<header>
+    <nav class="navbar navbar-expand navbar-light bg-light">
+        <div class="nav navbar-nav">
+            <a class="nav-item nav-link" href="#">Home</a>
+            <a class="nav-item nav-link" href="#">About</a>
+        </div>
+    </nav>
+</header>
